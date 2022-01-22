9a01feda9c1f2490252c6bf1b8d5415dc25c668b
stacherski <stacherski@gmail.com>
2022-01-22 19:29:32 +0100

    API KEY moved to .env

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,32 +1,35 @@
-import GoogleMapReact from "google-map-react"
-import LocationMarker from "./LocationMarker"
-import LocationInfoBox from "./LocationInfoBox"
+import GoogleMapReact from "google-map-react";
+import LocationMarker from "./LocationMarker";
+import LocationInfoBox from "./LocationInfoBox";
 
 const Map = ({ eventData, center, zoom }) => {
-
-    const markers = eventData.map((ev, index) => {
-            return <LocationMarker key={index} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}/>
-    })
+  const markers = eventData.map((ev, index) => {
     return (
-        <div className="map">
-            <GoogleMapReact
-                bootstrapURLKeys={{key : 'AIzaSyB7dhAVUjz_9v3LyefrQnz7kaz1YrEm7SA'}}
-                defaultCenter={center}
-                defaultZoom={zoom}
-            >
-            {markers}
-            </GoogleMapReact>
-        </div>
-    )
-
-}
+      <LocationMarker
+        key={index}
+        lat={ev.geometries[0].coordinates[1]}
+        lng={ev.geometries[0].coordinates[0]}
+      />
+    );
+  });
+  return (
+    <div className='map'>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+        defaultCenter={center}
+        defaultZoom={zoom}>
+        {markers}
+      </GoogleMapReact>
+    </div>
+  );
+};
 
 Map.defaultProps = {
-    center: {
-        lat: 51.127057,
-        lng: 16.9218248
-    },
-    zoom: 5
-}
+  center: {
+    lat: 51.127057,
+    lng: 16.9218248,
+  },
+  zoom: 5,
+};
 
-export default Map
+export default Map;
